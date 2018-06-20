8986d2efd86dc5f823b8758a0567edfb353effd4
TsarTechSoftware <porta-cube@porta-cubes-MacBook-Pro.local>
2018-06-20 06:13:52 -0700

    Filezilla not working at hotel

--- scripts/requests.js
+++ scripts/requests.js
@@ -15,15 +15,6 @@ $.ajax({
 	'</h5>\n' + articles["articles"][i]["description"] + '</div></div></div>';
 	}
 
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    	var outers = document.getElementsByClassName("posts");
-    	outers.style.width = '1100px';
-    	//outers.style.height = 'auto';
-    	//document.getElementsByClassName("innerPosts").style.padding = '3% 0%';
-    	//document.getElementsByClassName("postText").style.marginLeft = '0px';
-    	//document.getElementsByClassName("blogImages").style.cssFloat = 'none';
-	}
-
 	document.getElementById("blogPosts").innerHTML = response;
 	
 
