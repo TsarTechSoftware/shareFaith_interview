641edd1efd1b25fa21069cebb7f3f2f48e11b552
TsarTechSoftware <porta-cube@porta-cubes-MacBook-Pro.local>
2018-06-20 01:50:11 -0700

    JSON is being added

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,4 +1,4 @@
-var articleData = $.getJSON( "file:///Users/porta-cube/Desktop/dev/DeveloperNewHireProject/siteapp/data/articleData.json", function() {
+/*var articleData = $.getJSON( "file:///Users/porta-cube/Desktop/dev/DeveloperNewHireProject/siteapp/data/articleData.json", function() {
   console.log( "success" );
 })
   .done(function() {
@@ -9,8 +9,15 @@ var articleData = $.getJSON( "file:///Users/porta-cube/Desktop/dev/DeveloperNewH
   })
   .always(function() {
     console.log( "complete" );
-  });
-var articles = $.parseJSON(articleData);
+  });*/
+var articles = $.ajax({
+	type: 'GET',
+	url: 'data/articleData.json',
+	dataType: 'json',
+	success: function(data){
+		console.log("Success");
+	}
+});
 var response = "";
 var counter = 0;
 for(i in articles["articles"]){
@@ -22,5 +29,22 @@ for(i in articles["articles"]){
 	if(counter == 2){
 		break;
 	}
-}
+}/*{"articles": [
+		{
+			"photo":"images/article1.jpg",
+			"header":"The must-dos for church preperation",
+			"description":"How to get 10 times more organized leading up to Sunday"
+		},
+		{
+			"photo":"images/article2.jpg",
+			"header":"Get to see the top 10 mistakes you're making",
+			"description":"How technology is changing the church world! And how you are being left behind."
+		},
+		{
+			"photo":"images/article3.jpg",
+			"header":"Find out what other churches are doing",
+			"description":"How to get your church to thrive in the middle of nowhere.... and more!"
+		}
+	]
+}*/
 document.getElementById("blogPosts").innerHTML = response;
